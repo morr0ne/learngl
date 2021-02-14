f15573f11c3cb4834d111b386f9d9a3bea9f0116
Federico Morrone <contact@morrone.dev>
2021-02-15 00:38:07 +0100

    Improved error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context as AnyhowContext, Result};
 use bytemuck::bytes_of;
 use glfw::Context;
 use glow::HasContext;
@@ -52,8 +53,8 @@ impl Renderer {
     }
 }
 
-fn main() {
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+fn main() -> Result<()> {
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS)?;
 
     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
     glfw.window_hint(glfw::WindowHint::OpenGlProfile(
@@ -63,7 +64,7 @@ fn main() {
     // Create a windowed mode window and its OpenGL context
     let (mut window, events) = glfw
         .create_window(WIDTH, HEIGHT, WINDOW_TITLE, glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW window.");
+        .context("Failed to create glfw windows")?;
 
     // Make the window's context current
     window.make_current();
@@ -149,4 +150,6 @@ fn main() {
             glfw.poll_events();
         }
     }
+
+    Ok(())
 }
